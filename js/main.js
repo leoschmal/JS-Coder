52f963e88585d25c77f67081e93a28c1a24c0803
leo.schmal <leonardoschmal@gmail.com>
2021-09-04 21:46:34 -0300

    agrego metodo PUT para guardar en API los usuarios que se registren

--- js/main.js
+++ js/main.js
@@ -55,7 +55,8 @@ let Publicador2 = new Usuario(2, 'Jose Perez', 'Particular', 'Parana', 'email@em
 let Publicador3 = new Usuario(3, 'Caramagna', 'Inmobiliaria', 'Parana', 'email@email.com', '123456', 'Publicador', []);
 let Publicador4 = new Usuario(4, 'Alicia Reyes', 'Particular', 'Parana', 'email@email.com', '123456', 'Publicador', []);
 //declaro la variable global usuario que va a ser a la que le asigno los datos de quien inicie sesion
-let Usuario1 ;
+let Usuario1;
+let arrayUsuarios =[];
 //instancio algunos objetos inmueble, luego los agrego a un array de objetos
 let Departamento1 = new Inmueble(01, 'Casa', 'Parana', 'Vivienda', 2, 1, false, 22000, 4000, 'media/img/inmuebles/casa1t.jpg');
 let Departamento2 = new Inmueble(02, 'Departamento', 'Rosario', 'Vivienda', 3, 1, true, 33000, 7000, 'media/img/inmuebles/depto1t.jpg');
@@ -63,7 +64,7 @@ let Departamento3 = new Inmueble(03, 'Departamento', 'Parana', 'Vivienda', 1, 1,
 let Departamento4 = new Inmueble(04, 'Local', 'Cerrito', 'Comercial', 2, 1, false, 12000, 0, 'media/img/inmuebles/local1t.jpg');
 let Departamento5 = new Inmueble(05, 'Departamento', 'Villa Urquiza', 'Vivienda', 2, 1, false, 12000, 0, 'media/img/inmuebles/depto3t.jpg');
 const arrayDeptos = [Departamento1, Departamento2, Departamento3, Departamento4, Departamento5];
-let localidadesDisponibles=[];
+let localidadesDisponibles = [];
 
 /*cargo un json (base.json en root)con mas deptos publicados*/
 function leerJSon(archivo, callback) {
@@ -86,35 +87,52 @@ leerJSon("base.json", function (text) {
         let desdeJson = new Inmueble(arrayDeptos2[i].id, arrayDeptos2[i].tipo, arrayDeptos2[i].localidad, arrayDeptos2[i].uso, arrayDeptos2[i].habitaciones, arrayDeptos2[i].banios, arrayDeptos2[i].cochera, arrayDeptos2[i].monto, arrayDeptos2[i].expensas, arrayDeptos2[i].img1);
         arrayDeptos.push(desdeJson);
     }
-    //Leo el Json online (API)
+    //Leo el Json online de Inmuebles (API)
     const URLGET = "https://api.jsonbin.io/v3/b/61326fedc4352e1d076851c0/latest"
     let misDatos = [];
     $.get(URLGET, function (respuesta, estado) {
         if (estado === "success") {
             misDatos = respuesta.record;
-                       
-            }    
 
-    for (let j = 0; j < misDatos.length; j++) {        
-        let nn = new Inmueble(misDatos[j].id, misDatos[j].tipo, misDatos[j].localidad, misDatos[j].uso, misDatos[j].habitaciones, misDatos[j].banios, misDatos[j].cochera, misDatos[j].monto, misDatos[j].expensas, misDatos[j].img1);
-        arrayDeptos.push(nn);  
-        //cargo las localidades 
-        localidadesDisponibles.push(nn.localidad);     
-    }
+        }
+
+
+        for (let j = 0; j < misDatos.length; j++) {
+            let nn = new Inmueble(misDatos[j].id, misDatos[j].tipo, misDatos[j].localidad, misDatos[j].uso, misDatos[j].habitaciones, misDatos[j].banios, misDatos[j].cochera, misDatos[j].monto, misDatos[j].expensas, misDatos[j].img1);
+            arrayDeptos.push(nn);
+            //cargo las localidades 
+            localidadesDisponibles.push(nn.localidad);
+        }
         //console.log(arrayDeptos);      
-        
+
         //elimino localidades repetidas
         localidadesDisponibles = [...new Set(localidadesDisponibles)];
         //las cargo como opciones en menu desplegable de busqueda
-        for (i=0; i < localidadesDisponibles.length; i++){
+        for (i = 0; i < localidadesDisponibles.length; i++) {
             $('#localidadInmueble').append(`<option value="${localidadesDisponibles[i]}">`);
-            }
+        }
         cargoDeptos();
-});
+    });
 
 });
 
-
+//Leo el Json online de Usuarios (API)
+function leoUsuarios() {    
+    let URLGET_USERS = "https://api.jsonbin.io/b/6134015a470d3325940285b2/latest";
+    let misUsuarios = [];
+    $.get(URLGET_USERS, function (respuesta1, estado1) {
+        if (estado1 === "success") {
+            misUsuarios = respuesta1;
+            console.log("misUsuarios", misUsuarios);
+        }
+        for (let j = 0; j < misUsuarios.length; j++) {
+            let nn = new Usuario(misUsuarios[j].id, misUsuarios[j].nombre, misUsuarios[j].apellido, misUsuarios[j].localidad, misUsuarios[j].email, misUsuarios[j].telefono, misUsuarios[j].tipo, misUsuarios[j].publicaciones);
+            arrayUsuarios.push(nn);                        
+        }
+        console.log(arrayUsuarios);
+    });
+};
+leoUsuarios();
 
 //Eventos en formularios(cambie algunos a JQUERY)
 //const formInicio = document.getElementById("formularioInicio");
@@ -133,12 +151,12 @@ botonVolverDeBusqueda.on('click', funcionVolverDeBusqueda);
 botonVolverDePublicar.on('click', funcionVolverDePublicar);
 
 //muestra el valor del change en el form buscar
-$("#alqBuscar").change(function (e) { 
+$("#alqBuscar").change(function (e) {
     let valor = formato.format(e.target.value);
     $('#rangeAlq').text(`${valor}`)
 });
 
-$("#expBuscar").change(function (e) { 
+$("#expBuscar").change(function (e) {
     let valor = formato.format(e.target.value);
     $('#rangeExp').text(`${valor}`)
 });
@@ -187,7 +205,8 @@ function validarForm(e) {
         return;
     }
     //con los datos ingresados, instancio el objeto Usuario
-    Usuario1 = new Usuario(id, nombre, apellido, localidad, email, telefono, tipo, []);
+    let nuevoId = arrayUsuarios.length + 1;
+    Usuario1 = new Usuario(nuevoId, nombre, apellido, localidad, email, telefono, tipo, []);
     //formulario correcto
     mostrarMensaje("Datos ingresados correctamente", Usuario1);
 
@@ -197,6 +216,24 @@ function validarForm(e) {
 
     let avatar = document.getElementById("avatar");
     avatar.src = 'media/img/carpincho.svg';
+    arrayUsuarios.push(Usuario1);
+    console.log("agrego usuario", arrayUsuarios);
+    //-----------
+    let jj = JSON.stringify(arrayUsuarios);
+    $.ajax({
+        url: 'https://api.jsonbin.io/b/6134015a470d3325940285b2',
+        contentType: 'application/json',
+        method: 'PUT',
+        //XMasterKey: '$2b$10$JP7lQa.UN5cW6CuENZFXwefu.tNQ4cvGdj4scjZQejqb5n8XIcOXa',        
+        data: jj        
+    }).done(function () {
+        console.log('SUCCESS');
+    }).fail(function (msg) {
+        console.log('FAIL');
+    }).always(function (msg) {
+        console.log('ALWAYS');
+    });
+    //----------
 
 }
 
@@ -260,12 +297,12 @@ function mostrarMensaje(mensaje, usuario) {
         alerta.remove();
         //formInicio.remove();
         formInicio.addClass("ocultar");
-        
+
         if (usuario.tipo === "Publicador") {
             formPublicador.removeClass("ocultar");
         }
         if (usuario.tipo === "Buscador") {
-            formBuscador.removeClass("ocultar");            
+            formBuscador.removeClass("ocultar");
         }
 
     }, 2000);
@@ -356,7 +393,7 @@ function validarFormBusca(e) {
 /*----------------------------------Muestra los ultimo 10 deptos publicados al final del body-----------------------*/
 function cargoDeptos() {
     //saco los primeros 10 deptos cargados del total de publicaciones
-    let deptoMuestra = arrayDeptos.slice(0,10);
+    let deptoMuestra = arrayDeptos.slice(0, 10);
     let muestras = document.getElementById("contenedorMuestras");
     muestras.innerHTML = ` `;
     for (const inmueble of deptoMuestra) {
@@ -392,19 +429,19 @@ function muestroDeptos(objeto, localidad, tipoInmueble) {
     titulo.classList.remove("ocultar");
     //array para guardar lo filtrado
     let resultadoBusqueda = [];
-    
+
     //Filtro de búsqueda (por ahora dejo solo por localidad por razones de prueba, tengo pocos inmuebles cargados)
-    const filtroBusqueda = objeto.filter(busqueda => busqueda.localidad == localidad /*&& busqueda.tipo === tipoInmueble*/);
+    const filtroBusqueda = objeto.filter(busqueda => busqueda.localidad == localidad /*&& busqueda.tipo === tipoInmueble*/ );
     resultadoBusqueda = filtroBusqueda;
     //busqueda es el id de <section> dentro del cual quiero insertar las tarjetas  
     let resultBusq = document.getElementById("busqueda");
     //hago esto para resetear la seccion por si tenia busquedas anteriores
-    
+
     resultBusq.innerHTML = ` `;
     for (elemento of resultadoBusqueda) {
-        let ran1 = random(1,54);
-        let ran2 = random(1,54);
-        let ran3 = random(1,54);
+        let ran1 = random(1, 54);
+        let ran2 = random(1, 54);
+        let ran3 = random(1, 54);
         let contenedor = document.createElement("div");
         contenedor.classList.add('tarjetaDeptoBusqueda');
         contenedor.innerHTML = `<div class="moduloDatosTarjeta">
@@ -439,34 +476,34 @@ function muestroDeptos(objeto, localidad, tipoInmueble) {
         //console.log("detalles", detalles);
         //lo dejo a la escucha de un click y que ejecute la funcion mostrarDetallesBusqueda
         detalles.addEventListener('click', mostrarDetallesBusqueda);
-        
+
     }
 
 }
 //--------------modal para mostrar los detalles de la busqueda--------------------------//
-function mostrarDetallesBusqueda(e){
-        //guardo el id del boton
-        let id = e.target.id;
-        //expresion regular
-        var regex = /(\d+)/g;
-        //al id le dejo solo los nros con la regex
-        let nro = parseInt(id.match(regex));
-        //mis id de inmueble arrancan en 1, por eso el -1
-        let propiedad = arrayDeptos[nro - 1];
-        //paso el booleano a Si o No para mostrarlo
-        let cochera = propiedad['cochera'];
-        if (cochera) {
-            estacionamiento = 'Si';
-        } else {
-            estacionamiento = 'No';
-        }
-        //creo el div para mostrar en la tarjeta
-        let tarjetaDetalle = document.createElement("DIV");
-        //tarjetaDetalle.classList.add('tarjetaDeptoBusqueda');
-        tarjetaDetalle.classList.add('overlay');
-    
-        //Contenido de la tarjeta 
-        tarjetaDetalle.innerHTML = `<div class="detalles"><p class="parrafoDetalle">${propiedad.localidad}</p>
+function mostrarDetallesBusqueda(e) {
+    //guardo el id del boton
+    let id = e.target.id;
+    //expresion regular
+    var regex = /(\d+)/g;
+    //al id le dejo solo los nros con la regex
+    let nro = parseInt(id.match(regex));
+    //mis id de inmueble arrancan en 1, por eso el -1
+    let propiedad = arrayDeptos[nro - 1];
+    //paso el booleano a Si o No para mostrarlo
+    let cochera = propiedad['cochera'];
+    if (cochera) {
+        estacionamiento = 'Si';
+    } else {
+        estacionamiento = 'No';
+    }
+    //creo el div para mostrar en la tarjeta
+    let tarjetaDetalle = document.createElement("DIV");
+    //tarjetaDetalle.classList.add('tarjetaDeptoBusqueda');
+    tarjetaDetalle.classList.add('overlay');
+
+    //Contenido de la tarjeta 
+    tarjetaDetalle.innerHTML = `<div class="detalles"><p class="parrafoDetalle">${propiedad.localidad}</p>
         <h3>Tipo:${propiedad.tipo}</h3>  
         <p class="parrafoDetalle">Tipo de Uso: ${propiedad.uso}</p>    
         <p class="parrafoDetalle">Cantidad de habitaciones: ${propiedad.habitaciones}</p>
@@ -475,22 +512,22 @@ function mostrarDetallesBusqueda(e){
         <p class="parrafoDetalle">Alquiler Mesual: $ ${propiedad.monto}</p>
         <p class="parrafoDetalle">Expensas Aprox: $ ${propiedad.expensas}</p></div>
         <img class="imgDetalle" src=${propiedad.img1}></div>`;
-        //Lo tiro al body
-        const body = document.querySelector("body");
-        body.appendChild(tarjetaDetalle);
-    
-        //btn para cerrar
-        const cerrarTarjeta = document.createElement("P");
-        cerrarTarjeta.textContent = 'X';
-        cerrarTarjeta.classList.add('btnCerrar');
-        tarjetaDetalle.appendChild(cerrarTarjeta);
-    
-        //para cerrar la tarjeta
-        cerrarTarjeta.addEventListener('click', function () {
-            tarjetaDetalle.remove();
-        });
-    
-    }
+    //Lo tiro al body
+    const body = document.querySelector("body");
+    body.appendChild(tarjetaDetalle);
+
+    //btn para cerrar
+    const cerrarTarjeta = document.createElement("P");
+    cerrarTarjeta.textContent = 'X';
+    cerrarTarjeta.classList.add('btnCerrar');
+    tarjetaDetalle.appendChild(cerrarTarjeta);
+
+    //para cerrar la tarjeta
+    cerrarTarjeta.addEventListener('click', function () {
+        tarjetaDetalle.remove();
+    });
+
+}
 
 
 //--------------modal para mostrar el detalle de los deptos-----------------------------//
